7bd0e714c2de9b6d692021dae54d36faa7de000b
Evan Velek <evelek321@gmail.com>
2024-11-22 16:51:56 -0800

    New m2

--- query_finder.py
+++ query_finder.py
@@ -1,29 +1,50 @@
 from nltk.stem.porter import PorterStemmer
 from posting import Posting
+import math
+import time
+import csv
+
 STEMMER = PorterStemmer()
+NUM_DOCS = 55393
 
 
-def query(user_query : str, index):
+def process_line(line):
+    file_info = line.strip().split(' ')
+    if not file_info[0]:
+        return None
+    key = file_info[0]
+    postings = set()
+    doc_freq = 0
+    pairs = zip(file_info[1::2], file_info[2::2])
+    for doc, freq in pairs:
+        postings.add(Posting(doc, freq))
+        doc_freq += 1
+    return key, postings, doc_freq
+
+
+
+
+def query(user_query : str, index, doc_frequencies):
     query_list = user_query.split(" ")
     query_list = [STEMMER.stem(token.lower()) for token in query_list] # need to be stemmed to corresspond with index
 
         
-    response_ids = set()
-    if (query_list):
-        if (query_list[0] in index): # should change ordering in future
-            for post in index[query_list[0]]:
-                response_ids.add(post._doc_id)
-    
+    response_ids = {} # id : tf-idf score
     
     for i in range(0, len(query_list)):
-        if (not response_ids):
-            break
-        temp_ids = set()
         if (query_list[i] in index): # should change ordering in future
             for post in index[query_list[i]]:
-                temp_ids.add(post._doc_id)
-        
-            response_ids &= temp_ids
+                tf = post._word_count
+                df = doc_frequencies[query_list[i]]
+                idf = NUM_DOCS / df
+                log_tf = math.log10(tf) + 1
+                log_idf = math.log2(idf)
+                tf_idf = log_tf * log_idf
+
+                if (post._doc_id in response_ids):
+                    response_ids[post._doc_id] += tf_idf
+                else:
+                    response_ids[post._doc_id] = tf_idf
 
     return response_ids
 
@@ -39,37 +60,96 @@ def doc_id_reader(response_ids):
             doc_ids[str(count)] = cur
             count += 1
 
-    ret_set = set()
+    ret_dict = {}
     for id in response_ids:
-        ret_set.add(doc_ids[id].strip())
+        ret_dict[doc_ids[id].strip()] = response_ids[id]
 
-    return ret_set
+    return ret_dict
 
 if __name__ == '__main__':
     print(STEMMER.stem("ACM"))
     index = {}
-    with open('./index.txt') as index_file:
+    start_time = time.time()
+    # doc_frequency = dict.fromkeys(range(1000000))
+    doc_frequency = {}
+
+
+    '''with open('./index.csv', 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        count = 0
+        for row in reader:
+            print(count)
+            count += 1
+            key = row[0]
+            index[key] = set()
+            for i in range(1, len(row) - 1, 2):
+                index[key].add(Posting(row[i], row[i + 1]))'''
+
+    '''with open('./index.bin', 'rb') as index_file:
+        count = 0
         while True:
+            print(count)
+            count += 1
+            file_info = index_file.readline().decode('utf-8').split(' ')
+            if (file_info[0] == ''):
+                break
+            index[file_info[0]] = set()
+            doc_frequency[file_info[0]] = 0
+            for i in range(1, len(file_info) - 1, 2):
+                index[file_info[0]].add(Posting(file_info[i], file_info[i + 1]))
+                doc_frequency[file_info[0]] += 1'''
+
+    '''with open('./index.txt', 'r') as index_file:
+        count = 0
+        while True:
+            print(count)
+            count += 1
             file_info = index_file.readline().split(' ')
             if (file_info[0] == ''):
                 break
             index[file_info[0]] = set()
+            doc_frequency[file_info[0]] = 0
             for i in range(1, len(file_info) - 1, 2):
                 index[file_info[0]].add(Posting(file_info[i], file_info[i + 1]))
+                doc_frequency[file_info[0]] += 1'''
+    
+    with open('./index.txt', 'r') as index_file:
+        count = 0
+        total_file_info = index_file.readlines()
+        for file_info in total_file_info:
+            file_info = file_info.split(' ')
+            print(count)
+            count += 1
+            if (file_info[0] == ''):
+                break
+            index[file_info[0]] = set()
+            doc_frequency[file_info[0]] = 0
+            for i in range(1, len(file_info) - 1, 2):
+                index[file_info[0]].add(Posting(file_info[i], file_info[i + 1]))
+                doc_frequency[file_info[0]] += 1
+    
+    
+    end_time = time.time()
+    print(f'Time taken to create index: {end_time - start_time}')
+    print(f'Size of dictionary: {len(index)}')
 
     while True:
         query_inp = input("Query: ")
         if (not query_inp):
             break
 
-        resp = query(query_inp, index)
+        resp = query(query_inp, index, doc_frequency)
         ret = doc_id_reader(resp)
 
+        sorted_ret = dict(sorted(ret.items(), key=lambda item: item[1]))
+        reversed_sorted_ret = reversed(sorted_ret)
+
         count = 0
-        for a in ret:
+        for a in reversed_sorted_ret:
             if (count == 5):
                 break
             print(a)
             count += 1
+
 
             
